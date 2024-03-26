ce4b896defb0b7b7eb0de2449925b7d26f41bfb8
Fabio Gatto <fabio.gatto@soprasteria.com>
2024-03-26 17:19:02 +0100

    installation

---
+++ boot.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:$PATH;
+cd /home/@/xxx;
+node index.js > ../log.txt &
---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:$PATH;
+HOMEUSERNAME=`id -un`;
+cd /home/${HOMEUSERNAME};
+git clone https://github.com/niosz/cy4-fase0.git;
+cd cy4-fase0;
+npm install;
+cat boot.sh | sed -E 's/@/'"$HOMEUSERNAME"'/g' > boot.tmp && mv boot.tmp boot.sh;
+chmod +x boot.sh;
+echo "INSTALLED";
