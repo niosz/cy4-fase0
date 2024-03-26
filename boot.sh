cb4fda13059933461649eadd16c103b11bea7d1c
Fabio Gatto <fabio.gatto@soprasteria.com>
2024-03-26 18:37:02 +0100

    new4

--- boot.sh
+++ boot.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:$PATH;
-export PATH=/home/installdemo/.nvm/versions/node/v18.17.0:$PATH;
-cd /home/@/cy4-fase0;
-node index.js > ../log.txt & 
+export PATH=/home/installdemo/.nvm/versions/node/v18.17.0/bin:$PATH;
+cd /home/installdemo/cy4-fase0;
+node index.js > ../log.txt &
